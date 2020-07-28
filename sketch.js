6a9ace90f40ac7f5a70cbd5d006ec194233ea0fc
DesaiSwara <68277364+DesaiSwara@users.noreply.github.com>
2020-07-28 12:39:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,14 +6,32 @@ function setup() {
 
   speed=random(55,90);
   weight=random(400,1500);
-}
-
-function draw() {
-  background(0);  
 
   car=createSprite(20,100,70,40);
   car.velocityX=speed;
 
   wall=createSprite(1335,100,60,height/2);
+}
+
+function draw() {
+  background(0);
+
+  if (car.isTouching(wall)) {
+   car.velocityX=0;
+   var deformation=0.5 * weight * speed* speed/22509;
+  }
+
+  if (deformation>180) {
+    car.shapeColor=color(255,0,0);
+  }
+
+  if (deformation<180 && deformation>100) {
+    car.shapeColor=color(230,230,0);
+  }
+
+  if (deformation<100) {
+    car.shapeColor=color(0,255,0);
+  }
+
   drawSprites();
 }
